358100d7ece25c9cf45acd8f3899453703e00d54
Austin Osuide <austinosuide@austins-macbook-pro.osuide.com>
2025-12-23 22:26:22 +0000

    Fix migration revision ID length (varchar 32 limit)

--- backend/alembic/versions/032_add_mitre_threat_intelligence.py
+++ backend/alembic/versions/032_add_mitre_threat_intelligence.py
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision: str = "032_add_mitre_threat_intelligence"
+revision: str = "032_mitre_threat_intel"
 down_revision: Union[str, None] = "031_add_security_function"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
